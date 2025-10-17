36d2d6fc7a3d3cd5db7c33d3e913bd6e50b8ee9b
potionk <jongunkim.p@gmail.com>
2025-10-17 20:21:37 +0900

    feat: 웹페이지 폰트 noto sans로 변경

--- grove-frontend/tailwind.config.js
+++ grove-frontend/tailwind.config.js
@@ -3,7 +3,27 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: [
+          'Noto Sans KR',
+          'Noto Sans',
+          'ui-sans-serif',
+          'system-ui',
+          '-apple-system',
+          'Segoe UI',
+          'Roboto',
+          'Ubuntu',
+          'Cantarell',
+          'Helvetica Neue',
+          'Arial',
+          'Apple Color Emoji',
+          'Segoe UI Emoji',
+          'Segoe UI Symbol',
+          'Noto Color Emoji',
+        ],
+      },
+    },
   },
   plugins: [],
 };
